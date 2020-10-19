c10a71bdec0e444fceac277d9e7301b6c17e33f3
Rhi Batstone <60016148+rhi-batstone@users.noreply.github.com>
2020-10-19 13:01:13 +0100

    Update 0-Parameterised_reports.R
    Added a Test Raw Data section between lines 18-74 
    Includes 3 functions: 1-Check sheet names match, 2-Check for blanks in data, 3-Check for excel row limit
    
    Before merging - Should functions be in this script or sourced in separately?

--- 0-Parameterised_reports.R
+++ 0-Parameterised_reports.R
@@ -3,17 +3,75 @@ library(dplyr)
 
 start_time <- Sys.time()
 
-path <- "data/council-area-profiles-dataset.xlsx"
+path <- "data/council-area-profiles-dataset_test.xlsx"
 
 path %>%
   excel_sheets() %>%
   purrr::set_names() %>%
-  purrr::map(read_excel, path = path) %>%
+  purrr::map(read_excel, path = path)  %>%
   list2env(globalenv())
 
 updates %>% 
   as.list() %>%
   list2env(globalenv())
+
+# Test Raw Data  ==============================================================
+
+raw_data <- Filter(function(x) is(x, "data.frame"), mget(ls()))
+
+expected_file_names <-  c("updates",
+                           "population-estimates",
+                           "population-projections",
+                           "nature-of-population-change",
+                           "births-by-sex",
+                           "standardised-birth-rates",
+                           "births-by-age-of-mother",
+                           "fertility-rates",
+                           "deaths-by-sex",
+                           "standardised-death-rates",
+                           "deaths-by-sex-by-age",
+                           "leading-causes-of-death",
+                           "migration",
+                           "net-migration",
+                           "net-migration-rates",
+                           "life-expectancy",
+                           "marriages",
+                           "civil-partnerships",
+                           "household-estimates",
+                           "household-projections",
+                           "dwelling",
+                           "dwellings-by-type",
+                           "dwellings-by-council-tax-band")
+
+check_expected_data_files <- function(raw_data) {   #Check for expected data sheets
+  if(identical(sort(names(raw_data)), sort(expected_file_names)) == F){
+    stop("Unexpected Sheet Names")
+  }
+}
+
+
+check_blank_data <- function(raw_data) { #Check for NA's in data
+  
+  for(df in seq_along(raw_data)){
+    if(any(is.na(raw_data[[df]]))){
+      warning(paste("Blank Data in:", names(raw_data[df])))
+    }
+  }
+}
+
+
+check_row_limit <- function(raw_data) { #Check for row limit
+  for(df in seq_along(raw_data)){
+    if(nrow(raw_data[[df]]) == 1048575){
+      warning(paste("Excel Row limit reached:", names(raw_data[df])))
+    }
+  }
+}
+
+
+check_expected_data_files(raw_data)
+check_blank_data(raw_data)
+check_row_limit(raw_data)
 
 # Knit HTML documents =========================================================
 Area <- c(
