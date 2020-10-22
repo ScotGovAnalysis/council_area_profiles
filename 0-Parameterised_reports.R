5f1a161c3ba41aa31ab5c55fd84eca1119d9657d
Rhi Batstone <60016148+rhi-batstone@users.noreply.github.com>
2020-10-22 15:14:52 +0100

    Update File
    Made edits and added in test to check column names and if "Scotland" is in any columns named "Council area"

--- 0-Parameterised_reports.R
+++ 0-Parameterised_reports.R
@@ -17,61 +17,54 @@ updates %>%
 
 # Test Raw Data  ==============================================================
 
-raw_data <- Filter(function(x) is(x, "data.frame"), mget(ls()))
+# Creates a list of the sheets in the expected order
+raw_data <- path %>%
+  excel_sheets() %>%
+  purrr::set_names() %>%
+  purrr::map(read_excel, path = path)
 
-expected_file_names <-  c("updates",
-                           "population-estimates",
-                           "population-projections",
-                           "nature-of-population-change",
-                           "births-by-sex",
-                           "standardised-birth-rates",
-                           "births-by-age-of-mother",
-                           "fertility-rates",
-                           "deaths-by-sex",
-                           "standardised-death-rates",
-                           "deaths-by-sex-by-age",
-                           "leading-causes-of-death",
-                           "migration",
-                           "net-migration",
-                           "net-migration-rates",
-                           "life-expectancy",
-                           "marriages",
-                           "civil-partnerships",
-                           "household-estimates",
-                           "household-projections",
-                           "dwelling",
-                           "dwellings-by-type",
-                           "dwellings-by-council-tax-band")
+# Sources list of expected sheet names and the columns within them
+source("sourced/expected_names_and_columns.R")
 
-check_expected_data_files <- function(raw_data) {   #Check for expected data sheets
-  if(identical(sort(names(raw_data)), sort(expected_file_names)) == F){
-    stop("Unexpected Sheet Names")
+
+
+# Check for expected data sheets
+if (identical(names(raw_data), names(expected_names_and_columns)) == F){ 
+  stop(paste("Unexpected Sheet Names: ", setdiff(names(raw_data), names(expected_names_and_columns)), "\n"),
+       paste("Missing Sheet Names: ", setdiff(names(expected_names_and_columns), names(raw_data)), "\n"))
+  }
+
+# Check for NA's in data
+for(i in seq_along(raw_data)){
+  if(any(is.na(raw_data[[i]]))){
+    warning(paste("Blank Data in:", names(raw_data[i])))
   }
 }
 
+# Check for excels row limit. 
+# If it is exactly 1048576 rows it is likely there has been some data loss.
+for(i in seq_along(raw_data)){
+  if(nrow(raw_data[[i]]) == 1048575){ # 1 less than excel limit because R uses first row as column names
+    warning(paste("Excel Row limit reached:", names(raw_data[i])))
+  }
+}
 
-check_blank_data <- function(raw_data) { #Check for NA's in data
-  
-  for(df in seq_along(raw_data)){
-    if(any(is.na(raw_data[[df]]))){
-      warning(paste("Blank Data in:", names(raw_data[df])))
+# Check for expected column names 
+for(i in seq_along(raw_data)){
+  if(identical(names(raw_data[[i]]), expected_names_and_columns[[i]]) == F){
+    warning(paste("Unexpected Column Names", names(raw_data[i])))
+  }
+}
+
+# Check scotland in Council Areas 
+#TODO Find out if scotland is not meant to be in any tables (it's currently in all (except updates))
+ for(i in seq_along(raw_data)){
+   if("Council area" %in% names(raw_data[[i]])){ 
+     if(!("Scotland" %in% raw_data[[i]]$'Council area')){
+    warning(paste("Scotland expected but missing in:", names(raw_data[i])))
     }
   }
 }
-
-
-check_row_limit <- function(raw_data) { #Check for row limit
-  for(df in seq_along(raw_data)){
-    if(nrow(raw_data[[df]]) == 1048575){
-      warning(paste("Excel Row limit reached:", names(raw_data[df])))
-    }
-  }
-}
-
-
-check_expected_data_files(raw_data)
-check_blank_data(raw_data)
-check_row_limit(raw_data)
 
 # Knit HTML documents =========================================================
 Area <- c(
