3dcac4323ee9cf0795f2a24f2bc078f35505affd
Rhi Batstone <60016148+rhi-batstone@users.noreply.github.com>
2020-11-03 09:35:45 +0000

    Upload Data Validation file (#1)
    * Upload Data Validation file
    
    draft stage of raw data validation
    
    * Update 0-Parameterised_reports.R
    
    Added a Test Raw Data section between lines 18-74 
    Includes 3 functions: 1-Check sheet names match, 2-Check for blanks in data, 3-Check for excel row limit
    
    Before merging - Should functions be in this script or sourced in separately?
    
    * Tweak Code
    
    Changed path from test data to actual data
    
    * Delete data_validation.R
    
    No longer needed as code now in 0-Parameterised_reports.R
    
    * Update File
    
    Made edits and added in test to check column names and if "Scotland" is in any columns named "Council area"
    
    * Upload file
    
    Uploading source file for the expected data list
    
    * Creating Source Folder
    
    * Move File
    
    * Delete File
    
    * Update File
    
    Fixed incorrect file name
    
    * Remove a couple blank lines
    
    Co-authored-by: Joseph Adams <joseph.adams@nrscotland.gov.uk>

--- 0-Parameterised_reports.R
+++ 0-Parameterised_reports.R
@@ -15,6 +15,55 @@ updates %>%
   as.list() %>%
   list2env(globalenv())
 
+# Test Raw Data  ==============================================================
+
+# Creates a list of the sheets in the expected order
+raw_data <- path %>%
+  excel_sheets() %>%
+  purrr::set_names() %>%
+  purrr::map(read_excel, path = path)
+
+# Sources list of expected sheet names and the columns within them
+source("source/expected_names_and_columns.R")
+
+# Check for expected data sheets
+if (identical(names(raw_data), names(expected_names_and_columns)) == F){ 
+  stop(paste("Unexpected Sheet Names: ", setdiff(names(raw_data), names(expected_names_and_columns)), "\n"),
+       paste("Missing Sheet Names: ", setdiff(names(expected_names_and_columns), names(raw_data)), "\n"))
+  }
+
+# Check for NA's in data
+for(i in seq_along(raw_data)){
+  if(any(is.na(raw_data[[i]]))){
+    warning(paste("Blank Data in:", names(raw_data[i])))
+  }
+}
+
+# Check for excels row limit. 
+# If it is exactly 1048576 rows it is likely there has been some data loss.
+for(i in seq_along(raw_data)){
+  if(nrow(raw_data[[i]]) == 1048575){ # 1 less than excel limit because R uses first row as column names
+    warning(paste("Excel Row limit reached:", names(raw_data[i])))
+  }
+}
+
+# Check for expected column names 
+for(i in seq_along(raw_data)){
+  if(identical(names(raw_data[[i]]), expected_names_and_columns[[i]]) == F){
+    warning(paste("Unexpected Column Names", names(raw_data[i])))
+  }
+}
+
+# Check scotland in Council Areas 
+#TODO Find out if scotland is not meant to be in any tables (it's currently in all (except updates))
+ for(i in seq_along(raw_data)){
+   if("Council area" %in% names(raw_data[[i]])){ 
+     if(!("Scotland" %in% raw_data[[i]]$'Council area')){
+    warning(paste("Scotland expected but missing in:", names(raw_data[i])))
+    }
+  }
+}
+
 # Knit HTML documents =========================================================
 Area <- c(
   
