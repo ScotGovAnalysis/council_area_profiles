c7baf4295123aee3ae77772cb0c4bb4659cce99e
Joseph Adams <joseph.adams@nrscotland.gov.uk>
2021-03-19 10:52:52 +0000

    Rename scripts for clarity

---
+++ 1___create_all_profiles.R
@@ -0,0 +1,103 @@
+library(readxl)
+library(dplyr)
+
+start_time <- Sys.time()
+
+path <- "data/council-area-profiles-dataset.xlsx"
+
+path %>%
+  excel_sheets() %>%
+  purrr::set_names() %>%
+  purrr::map(read_excel, path = path) %>%
+  list2env(globalenv())
+
+updates %>% 
+  as.list() %>%
+  list2env(globalenv())
+
+source("11__check_data.R")
+
+# Knit HTML documents =========================================================
+Area <- c(
+  "Aberdeen City",
+  "Aberdeenshire",
+  "Angus",
+  "Argyll and Bute",
+  "City of Edinburgh",
+  "Clackmannanshire",
+  "Dumfries and Galloway",
+  "Dundee City",
+  "East Ayrshire",
+  "East Dunbartonshire",
+  "East Lothian",
+  "East Renfrewshire",
+  "Falkirk",
+  "Fife",
+  "Glasgow City",
+  "Highland",
+  "Inverclyde",
+  "Midlothian",
+  "Moray",
+  "Na h-Eileanan Siar",
+  "North Ayrshire",
+  "North Lanarkshire",
+  "Orkney Islands",
+  "Perth and Kinross",
+  "Renfrewshire",
+  "Scottish Borders",
+  "Shetland Islands",
+  "South Ayrshire",
+  "South Lanarkshire",
+  "Stirling",
+  "West Dunbartonshire",
+  "West Lothian"
+  )
+
+pb = txtProgressBar(min = 0, max = length(Area), initial = 0, style = 3)
+
+for (i in seq_along(Area)) {
+  rmarkdown::render("12__create_profile.Rmd",
+                    output_dir = "output",
+                    output_file = paste0(gsub(" ", "-", tolower(Area[[i]])),
+                                         "-council-profile.html"),
+                    params = list(area = Area[[i]]),
+                    quiet = ifelse(length(Area) > 1, TRUE, FALSE))
+  setTxtProgressBar(pb, i)
+}
+end_time <- Sys.time()
+
+print(run_time <- end_time - start_time)
+
+# All 32 council areas ========================================================
+# "Aberdeen City",
+# "Aberdeenshire",
+# "Angus",
+# "Argyll and Bute",
+# "City of Edinburgh",
+# "Clackmannanshire",
+# "Dumfries and Galloway",
+# "Dundee City",
+# "East Ayrshire",
+# "East Dunbartonshire",
+# "East Lothian",
+# "East Renfrewshire",
+# "Falkirk",
+# "Fife",
+# "Glasgow City",
+# "Highland",
+# "Inverclyde",
+# "Midlothian",
+# "Moray",
+# "Na h-Eileanan Siar",
+# "North Ayrshire",
+# "North Lanarkshire",
+# "Orkney Islands",
+# "Perth and Kinross",
+# "Renfrewshire",
+# "Scottish Borders",
+# "Shetland Islands",
+# "South Ayrshire",
+# "South Lanarkshire",
+# "Stirling",
+# "West Dunbartonshire",
+# "West Lothian"
