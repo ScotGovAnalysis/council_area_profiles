6a029e4f6aef3f0b7818cd711eea395e7a7549cb
Sam Hunter <sam@shunters.uk>
2023-06-05 09:23:08 +0100

    Top level parallelisation to produce all the markdown elements. Save them into RDS format. Rmd now reads the RDS file for data instead of passing in hundreds of megabytes as a parameter to the knitting process.

--- 11_create_all_profiles.R
+++ 11_create_all_profiles.R
@@ -1,14 +1,18 @@
 library(readxl)
-library(dplyr)
+library(tidyverse)
 library(purrr)
 library(testthat)
 library(parallel)
 library(rlist)
 
+
+# 
 # readline(paste("Are you following the update instructions?",
 #                "\n https://github.com/DataScienceScotland/council_area_profiles",
 #                "\n Hit enter to continue:"))
 
+# start a timer 
+st = Sys.time()
 
 source("functions folder/source functions.R")
 
@@ -19,45 +23,46 @@ expectations = set_expectations()
 check_expectations(raw_data, expectations)
 
 # There is a later dependency in the Rmd file where having the 
-# updates list at the top level of the global env is important.
+# updates sublist at the top level of the global env is important.
 raw_data = list.merge(raw_data, raw_data$updates)
+# this is a bandaid and needs to be changed 
+# TODO fix this shit
 
 # Knit HTML documents =========================================================
 Area <- c(
- "Aberdeen City")
-# ,
-#  "Aberdeenshire",
-#  "Angus",
-#  "Argyll and Bute",
-#  "City of Edinburgh",
-#  "Clackmannanshire",
-#  "Dumfries and Galloway",
-#  "Dundee City",
-#  "East Ayrshire",
-#  "East Dunbartonshire",
-#  "East Lothian",
-#  "East Renfrewshire",
-#  "Falkirk",
-#  "Fife",
-#  "Glasgow City",
-#  "Highland",
-#  "Inverclyde",
-#  "Midlothian",
-#  "Moray",
-#  "Na h-Eileanan Siar",
-#  "North Ayrshire",
-#  "North Lanarkshire",
-#  "Orkney Islands",
-#  "Perth and Kinross",
-#  "Renfrewshire",
-#  "Scottish Borders",
-#  "Shetland Islands",
-#  "South Ayrshire",
-#  "South Lanarkshire",
-#  "Stirling",
-#  "West Dunbartonshire",
-#  "West Lothian"
-# )
+ "Aberdeen City",
+ "Aberdeenshire",
+ "Angus",
+ "Argyll and Bute",
+ "City of Edinburgh",
+ "Clackmannanshire",
+ "Dumfries and Galloway",
+ "Dundee City",
+ "East Ayrshire",
+ "East Dunbartonshire",
+ "East Lothian",
+ "East Renfrewshire",
+ "Falkirk",
+ "Fife",
+ "Glasgow City",
+ "Highland",
+ "Inverclyde",
+ "Midlothian",
+ "Moray",
+ "Na h-Eileanan Siar",
+ "North Ayrshire",
+ "North Lanarkshire",
+ "Orkney Islands",
+ "Perth and Kinross",
+ "Renfrewshire",
+ "Scottish Borders",
+ "Shetland Islands",
+ "South Ayrshire",
+ "South Lanarkshire",
+ "Stirling",
+ "West Dunbartonshire",
+ "West Lothian"
+)
 
 
 # find the number of cpu cores we have to available
@@ -71,7 +76,7 @@ clusterExport(cl, varlist = c("produce_CA_data",
                               "produce_CA_plots", 
                               "produce_CA_tables",
                               "produce_CA_text",
-                              "raw_data")
+                              "raw_data"
                               ))
 # call any specific R commands across the nodes
 # here we call libraries to make sure the nodes can access tidyverse functions
@@ -94,7 +99,7 @@ clusterCall(cl, fun = function(){
 
 CA_data_list = parLapply(cl, Area, function(CA){
   
-  st = Sys.time()
+  
   # CA=Area[1]
   
   CA_data = produce_CA_data(CA, raw_data)
@@ -105,35 +110,29 @@ CA_data_list = parLapply(cl, Area, function(CA){
   
   CA_data = produce_CA_text(CA_data)
   
-  CA_data$timed_run = Sys.time() - st
+  write_rds(CA_data, file = paste0("temp/",CA_data$area,".rds"))
   
-  return(CA_data)
+  return(1)
   
 }
 )
 
-parLapply(cl, CA_data_list, function(CA_data){
+parLapply(cl, Area, function(area){
   
-  CA_data = CA_data_list[[1]]
+  # for debugging with a single CA
+  # area = Area[1]
   
   rmarkdown::render("31_create_profile_new.Rmd",
                     output_dir = "output",
-                    output_file = paste0(gsub(" ", "-", tolower(CA_data$area)),
+                    output_file = paste0(gsub(" ", "-", tolower(area)),
                                          "-council-profile.html"),
-                    params = list(area = CA_data$area,
-                                  data = CA_data),
-                    quiet = ifelse(length(Area) > 1, TRUE, FALSE))
+                    params = list(area = area),
+                    quiet = ifelse(length(area) > 1, TRUE, FALSE))
   
 })
 
-pb <- txtProgressBar(min = 0, max = length(Area), initial = 0, style = 3)
+stopCluster(cl)
 
-for (i in seq_along(Area)[1]) {
-  rmarkdown::render("31_create_profile.Rmd",
-                    output_dir = "output",
-                    output_file = paste0(gsub(" ", "-", tolower(Area[[i]])),
-                                         "-council-profile.html"),
-                    params = list(area = Area[[i]]),
-                    quiet = ifelse(length(Area) > 1, TRUE, FALSE))
-  setTxtProgressBar(pb, i)
-}
+timed_run = Sys.time() - st
+
+print(paste("Code complete. Run time:",timed_run))
